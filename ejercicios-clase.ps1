bb405f7b5eaa3c0ab74098e389600f29bcd6f3d9
Dgarrido <danielgarciagarrido96@gmail.com>
2025-03-11 23:17:07 +0100

    ejercicios-clase.ps1

--- ejercicios-clase.ps1
+++ ejercicios-clase.ps1
@@ -53,27 +53,26 @@ do{
     write-host "4. Division" 
     Write-Host "5. Exit"
     [int]$op = Read-Host "Introduce una opcion"
-
+    if($op -in 1..4){
+        [int]$num1 = read-host "introduce el 1º numero"
+        [int]$num = read-host "Introduce el 2º numero"
+    }
 switch ($op)
 {
-    '1'{[int]$num1 = read-host "introduce el 1º numero"
-        [int]$num = read-host "Introduce el 2º numero"
+    '1'{clear-host
         $tot= $num1 + $num
         Write-Host "La suma de los numeros  $num1 y $num es $tot" }
 
-    '2'{[int]$num1 = read-host "introduce el 1º numero"
-        [int]$num = read-host "Introduce el 2º numero"
+    '2'{clear-host
          $tot= $num1 - $num
          Write-Host "La resta de los numeros  $num1 y $num es $tot" }
 
-    '3'{[int]$num1 = read-host "introduce el 1º numero"
-        [int]$num = read-host "Introduce el 2º numero"
+    '3'{clear-host
         $tot= $num1 * $num
         Write-Host "La multiplicacion de los numeros  $num1 y $num es $tot" }
 
 
-    '4'{[int]$num1 = read-host "introduce el 1º numero"
-        [int]$num = read-host "Introduce el 2º numero"
+    '4'{clear-host
             if($num -eq 0){
             write-host "No se puede dividir entre 0"
             }else{
@@ -81,8 +80,74 @@ switch ($op)
             Write-Host "La division de los numeros  $num1 y $num es $tot" }
         }
         
-    '5' {Clear-Host}
+    '5' { read-host "Pulsa una tecla para cerrar"
+        Clear-Host}
 
     Default {{Write-Host "Debes de introducir una opcion correcta"}}
-}
+}    
 }until($op -eq 5)
+
+
+#Programa que le des una carpeta y te cuente los txt
+
+$carpeta = Read-Host "Introduce la carpeta"
+$archivos = Get-ChildItem $carpeta -Recurse -Include *.txt
+$cont = 0
+foreach($archivo in $archivos){
+    $cont++
+}
+write-host "Hay $cont archivos txt en la carpeta $carpeta"
+
+#El usuario comprueba un usuario, si existe que te lo diga si no existe pues te lo crea
+
+$usuario = Read-Host "Introduce el nombre de usuario"
+if(Get-LocalUser $usuario){
+    Write-Host "El usuario $usuario existe"
+}else{
+    New-LocalUser -name $usuario -NoPassword -FullName $usuario
+    write-host "El usuario $usuario ha sido creado"
+}
+
+#numeros pares del 0 al 100
+
+for ($i = 0; $i -lt 101; $i++)
+{
+    $tot= $i % 2
+    if ($tot -eq 0){
+        write-host "$i es par"
+    }
+}
+
+#numeros pares que tu le introduzcas por consola
+[int]$num=read-host "Introduce un numero inicial"
+[int]$num1=read-host "Introduce un numero final"
+
+if ($num -lt $num1) {
+    for ($i = $num; $i -le $num1; $i++)
+    {
+        $tot= $i % 2
+        if ($tot -eq 0){
+            write-host "$i es par"
+        }
+    }
+} else{
+    Write-Host "$num tiene que ser menor que $num1"
+}
+
+#Programa que me pida un numero de usuarios. A partir de este momneto solicitara n usuarios. Por cada uno de ellos comprobara que el usuario existe en el sistema.
+#Nota: Si el usuario no existe no debe de mostrar ningun error.
+$useri=@()
+[int]$usu=read-host "Introduce un numero de usuarios"
+for($i = 1; $i -le $usu; $i++){
+    $user=read-host "Introduce el usuario"$i
+   
+    $useri+=$user
+}
+foreach($user in $useri){
+    $dir=Get-LocalUser $user -ErrorAction SilentlyContinue
+    if($dir){
+        write-host "El usuario $user existe"
+    }
+}
+
+#
